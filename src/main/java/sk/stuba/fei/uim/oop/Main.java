eb1a67b2445fc9a8d73933485789adde5e83d4ee
xbokros <xbokros@stuba.sk>
2021-02-16 16:22:33 +0100

    Types

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -3,5 +3,17 @@ package sk.stuba.fei.uim.oop;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        int x = 15;
+        float y = 5.5f;
+        double z = 5.5;     //Always use double
+        long xyz = 5L;
+
+        char alpha = 'a';
+        String beta = "a";
+
+        int op1 = 2/3;      //0
+        int op2 = 2%3;      //2
+
     }
 }
